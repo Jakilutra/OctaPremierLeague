ea2109e9be646c5458fa36e9796c78c937896112
Jakilutra <jakilutra@gmail.com>
2019-07-03 11:36:04 +0100

    Update triggers.js

--- triggers.js
+++ triggers.js
@@ -9,6 +9,7 @@ function onEdit(e) {
       rule = {},
       rules = {},
       tierRange = {},
+      slot = {},
       slots = [],
       indvSlots = [],
       compSlots = [],
@@ -40,11 +41,12 @@ function onEdit(e) {
     return 39;
   }
   
-  // Showing and Hiding tier rows.
-  
   if (address === "E4" && sheet.getName() === "Config") {
+    
+    // Showing and Hiding tier rows.
+    
     firstHiddenRow = findHiddenRow(sheet);
-    tierNo = cell.getValue();
+    tierNo = cellValue;
     tierExp = firstHiddenRow - 19;
     tierDif = tierNo - tierExp;
     if (tierDif > 0){
@@ -53,38 +55,48 @@ function onEdit(e) {
     if (tierDif < 0){
       sheet.hideRows(firstHiddenRow+tierDif, -tierDif);
     }
+    
+    // Setting Slots Toggle validation on tier number change.
+  
+    slot = sheet.getRange("F4");
+    slots = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+    slots.splice(0, cellValue-1);
+    rule = SpreadsheetApp.newDataValidation().requireValueInList(slots).setAllowInvalid(false).build();
+    slot.setDataValidation(rule);
+    if (slot.getValue() < tierNo){
+      slot.setValue(tierNo);
+    }
+    
     // Defaulting individual slot toggles on tier number change.
     
+    slotSum = sheet.getRange("E18").getValue().match(/(\d\d?)/g);
     tierRange = sheet.getRange("E19:E38");
-    indvSlots = [[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1]];
+    indvSlots = tierRange.getValues();
+    while (tierDif > 0){
+      indvSlots[tierExp] = [1];
+      tierDif--;
+      tierExp++;
+    }
+    if ((slotSum + tierDif) > slot.getValue()) {
+      indvSlots = [[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1]];
+    }
     compSlots = [[""],[""],[""],[""],[""],[""],[""],[""],[""],[""],[""],[""],[""],[""],[""],[""],[""],[""],[""],[""]];
     compSlots.splice(20-tierNo);
     indvSlots.splice(tierNo, 20-tierNo);
     indvSlots = indvSlots.concat(compSlots);
     tierRange.setValues(indvSlots);
     
-    // Defaulting individual slot toggles validation on tier number change.
+    // Updating individual slot toggles validation on tier number change.
     
-    slotSum = sheet.getRange("E18").getValue();
-    slotSum = slotSum.match(/(\d\d?)/g);
-    slotRem = sheet.getRange("F4").getValue() - slotSum;
+    slotSum = sheet.getRange("E18").getValue().match(/(\d\d?)/g);
+    slotRem = slot.getValue() - slotSum;
     rules = tierRange.getDataValidations();
     for (index=0;index<20;index++) {
-      slots = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20].slice(0,slotRem+1);
+      indvRem = sheet.getRange("E"+(index+19)).getValue()-1;
+      slots = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20].slice(0,slotRem+indvRem+1);
       rules[index][0] = SpreadsheetApp.newDataValidation().requireValueInList(slots).setAllowInvalid(false).build();
     }
     tierRange.setDataValidations(rules);
-                                         
-    // Setting Slots Toggle on tier number change.
-  
-    cell = sheet.getRange("F4");
-    slots = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-    slots.splice(0, sheet.getRange("E4").getValue()-1);
-    rule = SpreadsheetApp.newDataValidation().requireValueInList(slots).setAllowInvalid(false).build();
-    cell.setDataValidation(rule);
-    if (cell.getValue() < tierNo){
-      cell.setValue(tierNo);
-    }
   }
   
   if (address === "F4" && sheet.getName() === "Config") {
